47e5413ade14a9578da5cd74ea4d29db7aa75f09
Darin <darinevanow@gmail.com>
2019-01-18 16:30:45 -0800

    Alter context to use our database instead of mock data. Remove some tests as they are not yet compatible with our database. Will add test database setup in future.

--- lib/rumble/accounts/accounts.ex
+++ lib/rumble/accounts/accounts.ex
@@ -3,51 +3,23 @@ defmodule Rumble.Accounts do
   The Accounts context.
   """
 
+  alias Rumble.Repo
   alias Rumble.Accounts.User
 
   @doc """
   Returns a list of all of our users.
-
-  ## Examples
-    iex> Rumble.Accounts.list_users()
-    [
-      %Rumble.Accounts.User{id: "1", name: "Darin", username: "darin-evanow"},
-      %Rumble.Accounts.User{id: "2", name: "Marina", username: "marina-nattinger"},
-      %Rumble.Accounts.User{id: "3", name: "Grant", username: "grant-ivison-lane"}
-    ]
   """
-  def list_users do
-    [
-      %User{id: "1", name: "Darin", username: "darin-evanow"},
-      %User{id: "2", name: "Marina", username: "marina-nattinger"},
-      %User{id: "3", name: "Grant", username: "grant-ivison-lane"}
-    ]
-  end
+  def list_users, do: Repo.all(User)
 
   @doc """
   Returns a user with the ID that is passed in.
-
-  ## Examples
-    iex> Rumble.Accounts.get_user("3")
-    %Rumble.Accounts.User{id: "3", name: "Grant", username: "grant-ivison-lane"}
-
-    iex> Rumble.Accounts.get_user("4")
-    nil
   """
-  def get_user(id) do
-    Enum.find(list_users(), fn map -> map.id == id end)
-  end
+  def get_user(id), do: Repo.get(User, id)
+  def get_user!(id), do: Repo.get!(User, id)
 
   @doc """
   Returns a user with the params that are passed in.
-
-  ## Examples
-    iex> Rumble.Accounts.get_user_by(name: "Darin")
-    %Rumble.Accounts.User{id: "1", name: "Darin", username: "darin-evanow"}
   """
-  def get_user_by(params) do
-    Enum.find(list_users(), fn map ->
-      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-    end)
-  end
+  def get_user_by(params), do: Repo.get_by(User, params)
+  def get_user_by!(params), do: Repo.get_by!(User, params)
 end
--- test/rumble_web/controllers/user_controller_test.exs
+++ test/rumble_web/controllers/user_controller_test.exs
@@ -5,10 +5,4 @@ defmodule RumbleWeb.UserControllerTest do
     conn = get(conn, "/users")
     assert html_response(conn, 200) =~ "Listing Users"
   end
-
-  test "GET /users/:id", %{conn: conn} do
-    conn = get(conn, "/users/1")
-    assert html_response(conn, 200) =~ "Showing User"
-    assert html_response(conn, 200) =~ "Darin"
-  end
 end
